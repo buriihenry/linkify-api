eda7d65c7879caba93d9335a2d0eb1dc7d68ae72
buriihenry <buriihenry@gmail.com>
2024-12-01 11:25:02 +0300

    retrive a single page

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request
 import validators
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask.json import jsonify
-from src.constants.http_status_code import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_201_CREATED, HTTP_200_OK
+from src.constants.http_status_code import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_201_CREATED, HTTP_200_OK, HTTP_404_NOT_FOUND
 from src.database import Bookmark, db
 
 bookmarks = Blueprint("bookmarks", __name__, url_prefix="/api/v1/bookmarks")
@@ -74,3 +74,27 @@ def handle_bookmarks():
         }    
         
         return jsonify({'data':data, "meta": meta}), HTTP_200_OK
+    
+@bookmarks.get("/<int:id>")
+@jwt_required()
+def get_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({'messgae':'Item not found'}), HTTP_404_NOT_FOUND
+    
+    return jsonify({
+        
+        'id':bookmark.id,
+        'url': bookmark.url,
+        'short_url':bookmark.short_url,
+        'visit':bookmark.visits,
+        'body': bookmark.body,
+        'created_at': bookmark.created_at,
+        'updated_at':bookmark.updated_at,
+    }), HTTP_200_OK
+    
+    
+        
